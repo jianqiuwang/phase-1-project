bc3b44845f86d38c9ca6d2688a9ec79deca777d0
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-07-28 20:45:39 -0500

    adding DOMContentLoaded event and prevent default behavior with event.preventDefault()

---
+++ script.js
@@ -0,0 +1,8 @@
+const input = document.getElementById('input')
+const search_btn = document.getElementById('search_btn')
+const save_btn = document.getElementById('save_btn')
+
+document.addEventListener('DOMContentLoaded',e=>{
+    e.preventDefault()
+})
+
